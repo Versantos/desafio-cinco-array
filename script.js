bf58aee9df764932d157b2d8c46ada5613f7754f
Versantos <veronicasilvamagalhaes@gmail.com>
2022-01-26 23:37:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let roupa=["saia","vestido","calça","blusa","top"]
+    console.log(roupa)
+
+    roupa.splice(0,0, "bermuda")
+    console.log(roupa)
+
+    roupa.shift()
+    console.log(roupa)
+
+    roupa.pop()
+    console.log(roupa)
+
+    roupa.splice(4,0, "chinelo","sapato")
+
+    let numbers=[8,6,3,7,5,9,2,1,4]
+    console.log(numbers.sort())
